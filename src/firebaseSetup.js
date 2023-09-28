796d0beee0ae7f6db2ccb34df4496caad672fc5c
Jane-Dwayne <142972919+Jane-Dwayne@users.noreply.github.com>
2023-09-28 09:46:55 -0700

    Update firebaseSetup.js

--- src/firebaseSetup.js
+++ src/firebaseSetup.js
@@ -14,12 +14,12 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase
 // };
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDf9ZvBdhn7vEV2ZVkUe5aFrCvJy1EvSac",
-    authDomain: "onlinetesting-96dd3.firebaseapp.com",
-    databaseURL: "https://onlinetesting-96dd3.firebaseio.com",
-    projectId: "onlinetesting-96dd3",
-    storageBucket: "onlinetesting-96dd3.appspot.com",
-    messagingSenderId: "855612276612"
+    apiKey: "AIzaSyBYpXYRyIRdN8o0GyfVewSdUM2P3Q-tUb4",
+    authDomain: "onlinetesting-42b6b.firebaseapp.com",
+    databaseURL: "https://onlinetesting-42b6b-default-rtdb.firebaseio.com",
+    projectId: "onlinetesting-42b6b",
+    storageBucket: "onlinetesting-42b6b.appspot.com",
+    messagingSenderId: "303642679764"
 }
 
 // Initialize Firebase with the config object
